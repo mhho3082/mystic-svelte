646106ab44d9f29bc82539ba519ffc3c6200a969
Max Ho <73323597+mhho3082@users.noreply.github.com>
2023-11-10 22:51:14 +0800

    FIX: Build into /docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), viteSingleFile()],
+  build: {
+    outDir: 'docs'
+  }
 });
